b96808cb3463ec62f658f52d43f93415c7cfc265
williamtonnetwork <jancoskillas@gmail.com>
2024-05-21 22:59:09 +0700

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+const headingTitle = document.querySelector(".banner .heading-block .title");
+const valueTitle = headingTitle.getAttribute("data-title");
+const valueArray = valueTitle.split("");
+headingTitle.innerHTML = valueArray
+  .map((item) => {
+    return `<span>${item}</span>`;
+  })
+  .join("");
+
+headingChilds = document.querySelectorAll(".banner .heading-block .title span");
+headingChilds.forEach((child, index) => {
+  child.style.animation = `upDown2 ease-in-out 5s ${
+    index * 0.25
+  }s infinite alternate-reverse`;
+});
